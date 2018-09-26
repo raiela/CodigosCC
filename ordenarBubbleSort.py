3c2c7980fd3774b073961f598425b5c718612aaa
Raiela Quirino <36709531+raiela@users.noreply.github.com>
2018-09-25 22:00:37 -0300

    Add files via upload

---
+++ ordenarBubbleSort.py
@@ -0,0 +1,15 @@
+lista = list(map(int, input("Digite a lista a ser ordenada: ").split()))
+menor = lista[0]
+aux = 0
+cont = 0
+for i in range(len(lista)):
+	for k in range(len(lista)):
+		if(lista[i] <= lista[k]):
+			aux = lista[k]
+			lista[k] = lista[i]
+			lista[i] = aux
+
+print(lista)
+
+
+
