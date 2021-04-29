9ccd8f1a7aeebd0509461bb0d19e07a5dbb8ba4b
Raiela <raielaq.l@hotmail.com>
2021-04-29 13:39:24 -0300

    Melhoria q2

--- PE_5Periodo/S8/q2.py
+++ PE_5Periodo/S8/q2.py
@@ -7,12 +7,15 @@ ob1 = [386, 850, 1544, 3035, 6650, 13887, 28059, 50916]
 ob2 = [375, 805, 1644, 3123, 6839, 14567, 27439, 52129]
 ob3 = [393, 824, 1553, 3235, 6768, 13456, 27659, 51360]
 
-m, b, r_value, p_value, std_err = stats.linregress(size , ob1)
+obMedia = [384.66,826.33, 1580.33, 3131.33, 6722.33, 13970, 27719, 51468.33]
+
+m, b, r_value, p_value, std_err = stats.linregress(size , obMedia)
 
 print(m, b)
 
-plt.plot(size, ob1)
-# plt.plot(size, ob2)
-# plt.plot(size, ob3)
+plt.plot(size, ob2)
+plt.plot(size, ob2)
+plt.plot(size, ob3)
+plt.plot(size, obMedia)
 
 plt.show()
